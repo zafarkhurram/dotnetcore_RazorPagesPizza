1070dfaccc2130c72ba43c6782515adde0845f1a
Zafar <Khurram.Zafar1@afiniti.com>
2022-09-04 01:18:21 +0300

    Learning ASP.NET Core tutorial from Microsoft

---
+++ Pages/Pizza.cshtml.cs
@@ -0,0 +1,32 @@
+using dotnetcore_RazorPagesPizza.Models;
+using dotnetcore_RazorPagesPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnetcore_RazorPagesPizza.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = new();
+        public List<Pizza> pizzas = new();
+        public void OnGet()
+        {
+            pizzas = PizzaService.GetAll();
+        }
+        public IActionResult OnPost() {
+            if (!ModelState.IsValid) {
+                return Page();
+            }
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+        public IActionResult OnPostDelete(int id) {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+        public string GlutenFreeText(Pizza pizza) {
+            return pizza.IsGlutenFree ? "Gluten Free": "Not Gluten Free";
+        }
+    }
+}
